cf34f3086de916c69dfa410cd783ec9accbb66ca
hyeon81 <guswly801@gmail.com>
2023-09-08 01:56:42 +0900

    add page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
-import Image from 'next/image'
-import styles from './page.module.css'
 import {Container} from "@mui/material";
 
-export default function Home() {
+export async function fetchData(){
+  const res = await fetch('https://picsum.photos/200')
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+  return res.json()
+}
+export default async function Home() {
+  const data = await fetchData();
+  console.log("data", data);
+  if (!data) return <div>loading...</div>;
   return (
-    <Container>
-      <img alt={"이미지"}/>
-        <div>설명</div>
-        <div>장소</div>
-    </Container>
+    <div>
+      <h1>Home</h1>
+      <div>{data}</div>
+    </div>
   )
 }
